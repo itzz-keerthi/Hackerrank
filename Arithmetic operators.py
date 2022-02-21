f476bd3c1666ff8e2b16900f80ff42ccee28e469
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-02-21 08:15:55 +0530

    Create Arithmetic operators.py

---
+++ Arithmetic operators.py
@@ -0,0 +1,23 @@
+#Python program to deal with arithmetic operators
+'''PROBLEM STATEMENT
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Example:
+a=3
+b=5
+output:   Explanation
+8         3+5
+-2        3-5
+15        3*5'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
