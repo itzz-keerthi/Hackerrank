802f847985aec34f96732596085066ffe718623c
Keerthana J <98206044+itzz-keerthi@users.noreply.github.com>
2022-03-23 20:13:34 +0530

    Basic Data Types

---
+++ Basic Data Types.cpp
@@ -0,0 +1,35 @@
+/*TASK
+Input Format
+
+Input consists of the following space-separated values: int, long, char, float, and double, respectively.
+
+Output Format
+
+Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+
+Sample Input
+3 12345678912345 a 334.23 14049.30493
+
+Sample Output
+3
+12345678912345
+a
+334.230
+14049.304930000  */
+
+#include <iostream>
+#include<iomanip>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl<<fixed<<setprecision(3)<<d<<endl<<fixed<<setprecision(9)<<e;
+    return 0;
+}
